7d1ba150721b374d18b5e11efcf2a3e6754c0fe3
ASR Logistics <aarti19830@gmail.com>
2023-01-12 18:39:25 +0530

    more crypti

---
+++ cryptography/main.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+using namespace std;
+int main()
+{
+    a:
+    system("cls");
+    cout<<"______________________________________________________________________________________________________________"<<endl<<endl;
+    cout<<"______________________________________________________CRYPTOGRAPHY____________________________________________"<<endl<<endl;
+    cout<<"______________________________________________________________________________________________________________"<<endl;
+    cout<<"|"<<endl;
+    cout<<"____________________1) ENCODE"<<endl;
+    cout<<"|"<<endl;
+    cout<<"____________________2) DECODE"<<endl;
+    int ch;
+    cout<<"heck@crypto.graphy:-$~ ";
+    cin>>ch;
+    if(ch==1){
+        system("cls");
+    }
+    else if(ch==2){
+        system("cls");
+    }
+    else{
+        goto a;
+    }
+    getch();
+    return 0;
+}
