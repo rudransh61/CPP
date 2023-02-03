ca569e31ab9aee2622ca3ab9f929d8c2060db571
ASR Logistics <aarti19830@gmail.com>
2023-02-03 17:52:44 +0530

    caeser

--- cryptography/main.cpp
+++ cryptography/main.cpp
@@ -1,32 +1,49 @@
-#include<bits/stdc++.h>
-#include<conio.h>
-#include<iostream>
-#include<stdio.h>
-#include<cmath>
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
 using namespace std;
+
+string caesar(string text, int s)
+{
+    string result = "";
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (isupper(text[i]))
+            result += char(int(text[i] + s - 65) % 26 + 65);
+        else
+            result += char(int(text[i] + s - 97) % 26 + 97);
+    }
+    return result;
+}
+
 int main()
 {
-    a:
-    system("cls");
-    cout<<"______________________________________________________________________________________________________________"<<endl<<endl;
-    cout<<"______________________________________________________CRYPTOGRAPHY____________________________________________"<<endl<<endl;
-    cout<<"______________________________________________________________________________________________________________"<<endl;
-    cout<<"|"<<endl;
-    cout<<"____________________1) ENCODE"<<endl;
-    cout<<"|"<<endl;
-    cout<<"____________________2) DECODE"<<endl;
-    int ch;
-    cout<<"heck@crypto.graphy:-$~ ";
-    cin>>ch;
-    if(ch==1){
-        system("cls");
+    while (true)
+    {
+        cout << "================Cryptography All ciphers with Encoder and Decoder================= \n";
+        cout << "1)Caesar Cipher";
+        int ch1;
+        cout << "Choose the Cipher >> ";
+        cin >> ch1;
+        cout<<endl;
+        if (ch1 == 1)
+        {
+            string msg;
+            cout << "Enter message : \n >>";
+            cin >> msg;
+            cout<<"Enter cipher number : \n>>";
+            int dis;
+            cin>>dis;
+            string code = caesar(msg,dis);
+            cout << "Your Coded message :>>" << code << endl;
+        }
+        else
+        {
+            cout << "Wrong input , Abort !!" << endl;
+            return 0;
+        }
     }
-    else if(ch==2){
-        system("cls");
-    }
-    else{
-        goto a;
-    }
-    getch();
     return 0;
 }
