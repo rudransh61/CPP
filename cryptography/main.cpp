0bdd6cde95b63292d38cf1c1779ce241b568598a
ASR Logistics <aarti19830@gmail.com>
2023-02-10 17:51:39 +0530

    atbash

--- cryptography/main.cpp
+++ cryptography/main.cpp
@@ -17,19 +17,51 @@ string caesar(string text, int s)
     }
     return result;
 }
+char upper_case[]={'Z', 'Y', 'X', 'W', 'V', 'U',
+ 				   'T', 'S', 'R', 'Q', 'P', 'O',
+				   'N', 'M', 'L', 'K', 'J', 'I', 
+				   'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+
+// Creating a array of lower case alphabets				   
+ char lower_case[]={'z', 'y', 'x', 'w', 'v', 'u',
+  					't', 's', 'r', 'q', 'p', 'o',
+					'n', 'm', 'l', 'k', 'j', 'i',
+					'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};     
+
+// Encryption and Decryption function
+string atbash(string message)
+{
+	int len = message.size();                   // Calculating lenght of the string
+	string cipher;
+	int ascii_char;
+	for(int i=0;i<len;i++)
+	{
+		
+		ascii_char = message[i];                         // Storing ASCII value of alphabet
+		if(ascii_char>='A' && ascii_char<='Z')
+		cipher = cipher + upper_case[ascii_char-65];    // If character is in upper case(minus 65 from its ASCII value to get reversed character)
+		else
+		cipher = cipher + lower_case[ascii_char-97];    // If character is in upper case(minus 97 from its ASCII value to get reversed character)
+			
+	}
+	return cipher;
+}
 
 int main()
 {
     while (true)
     {
+        system("cls");
         cout << "================Cryptography All ciphers with Encoder and Decoder================= \n";
-        cout << "1)Caesar Cipher";
+        cout << "1)Caesar Cipher"<<endl;
+        cout << "2)Atbash Cipher"<<endl;;
         int ch1;
         cout << "Choose the Cipher >> ";
         cin >> ch1;
-        cout<<endl;
+
         if (ch1 == 1)
         {
+                    cout<<endl;
             string msg;
             cout << "Enter message : \n >>";
             cin >> msg;
@@ -39,11 +71,22 @@ int main()
             string code = caesar(msg,dis);
             cout << "Your Coded message :>>" << code << endl;
         }
+        else if (ch1 == 2)
+        {
+                    cout<<endl;
+            string msg;
+            cout << "Enter message : \n >>";
+            cin >> msg;
+            string code = atbash(msg);
+            cout << "Your Coded message :>>" << code << endl;
+        }
         else
         {
             cout << "Wrong input , Abort !!" << endl;
             return 0;
         }
+        int chh;
+        cin>>chh;
     }
     return 0;
 }
